21d9cdc9e34001f2c6e20a685c50105ea35ae6a6
JoseMurillo2711 <93305163+JoseMurillo2711@users.noreply.github.com>
2025-07-09 15:11:37 +0000

    UPD: guia 15

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import ChartUI from './components/ChartUI';
 import { Grid } from '@mui/material';
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
 
 
 
@@ -26,7 +27,7 @@ function App() {
       <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center"><AlertUI description="No se preveen lluvias" /></Grid>
 
       {/* Selector */}
-      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+      <Grid size={{ xs: 12, md: 3 }}><SelectorUI/></Grid>
 
       {/* Indicadores */}
       <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
